57df71757d9ee3d08cd8233123fd54d8ca7c1a0f
Adonis Veneros <adonisiv@iestptrujillo.net>
2024-09-19 11:01:48 -0500

    Mi primer commit

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,60 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet("DevolverAlumno")]
+        public string DevolverAlumno()
+        {
+            string mensaje = string.Empty;
+            Alumno Alumno1 = new Alumno();
+            Alumno Alumno2 = new Alumno("Juan", "Perez", "62152820");
+            return Alumno2.DevolverNombreCompleto();
+        }
+
+        [HttpGet("DevolverDocumento")]
+        public string DevolverDocumento()
+        {
+            string mensaje = string.Empty;
+            Alumno Alumno1 = new Alumno();
+            Alumno Alumno2 = new Alumno("Juan", "Perez", "62152820");
+            return Alumno2.DevolverDocumento();
+        }
+
+        [HttpGet("DevolverEdad")]
+        public int DevolverEdad()
+        {
+            string mensaje = string.Empty;
+            Alumno Alumno3 = new Alumno("Adonis", "Veneros", "62152820", new DateTime(2003, 04, 21));
+            return Alumno3.DevolverEdad();
+        }
+    }
+}
+  
---
+++ Controllers/alumno.cs
@@ -0,0 +1,48 @@
+﻿namespace WebApplication1.Controllers
+{
+    public class Alumno
+    {
+        //creando propiedades
+        string nombre;
+        string apellido;
+        string dni;
+        DateTime fechanacimiento;
+
+        public Alumno()
+        {
+            nombre = string.Empty;
+            apellido = string.Empty;
+            dni = string.Empty;
+            fechanacimiento = DateTime.Now;
+        }
+        //constructor con parametros
+        public Alumno(string pnombre, string papellido, string pdni)
+        {
+            nombre = pnombre;
+            apellido = papellido;
+            dni = pdni;
+        }
+
+        public Alumno(string pnombre, string papellido, string pdni, DateTime pfechanaciemineto)
+        {
+            nombre = pnombre;
+            apellido = papellido;
+            dni = pdni;
+            fechanacimiento = pfechanaciemineto;
+        }
+        public string DevolverNombreCompleto()
+        {
+            return nombre + " " + apellido;
+        }
+
+        public string DevolverDocumento()
+        {
+            return dni;
+        }
+
+        public int DevolverEdad()
+        {
+            return DateTime.Now.Year - fechanacimiento.Year;
+        }
+    }
+}
